66a288f4b03e27ebfe954810f655507c3492b182
felix <felixmendoza45678@gmail.com>
2025-02-09 10:12:01 -0400

    cliente crud

--- Modules/clientesModulos.php
+++ Modules/clientesModulos.php
@@ -6,9 +6,19 @@ function GuardarClientesModulo($link,$nombre,$apellido,$telefono,$correo,$direcc
     mysqli_query($link,$query);
 }
 
-function CrearPrestamoModulo($link,$idCliente,$monto,$cuotas,$mensaje,$fechaconcesion)
+function CrearPrestamoModulo ($link,$cliente_id,$monto,$cuotas,$mensaje,$fecha_concesion)
 {
+    $query = "INSERT INTO prestamos (id_cliente, monto,cuotas,mensaje,fecha_concesion) VALUES ('$cliente_id', '$monto','$cuotas','$mensaje','$fecha_concesion')";
+    mysqli_query($link,$query);
+}
 
+function CrearCalendarioDePagos ($link,$id_prestamo,$montocuotas,$fechas_pago,$cuotas)
+{ 
+    for ($i=1; $i < $cuotas; $i++) { 
+        $fecha = $fechas_pago[$i-1];
+        $query = "INSERT INTO calendario_pagos (id_prestamo, numero_cuota, fecha_vencimiento, monto) VALUES ('$id_prestamo','$montocuotas','$fecha','$montocuotas')";
+        mysqli_query($link,$query);
+    }
 }
 
 ?>
